6f94af1c5a7105b2be8c8d409af1da0b6fb6eb47
santiagoropero <santiagoropero2001@gmail.com>
2022-05-22 19:53:16 -0500

    Creación de componentes para módulo de estudiante

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateStudentComponent } from './create-student/create-student.component';
+import { ListStudentComponent } from './list-student/list-student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateStudentComponent,
+    ListStudentComponent
   ],
   imports: [
     BrowserModule,
