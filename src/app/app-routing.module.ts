71a64821f2fd4f10e84469e4f0ac1930642feef9
Spartan2448 <71468930+Spartan2448@users.noreply.github.com>
2022-05-23 09:30:31 -0500

    Vista Registro estudiante
    "creacion vista diseño registrar estudiante"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateStudentComponent } from './create-student/create-student.component';
 import {ListStudentComponent} from './list-student/list-student.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/students', pathMatch: 'full'},
-  {path: 'students', component: ListStudentComponent}
+  {path: 'students', component: ListStudentComponent},
+  {path: 'create', component: CreateStudentComponent}
 ];
 
 @NgModule({
