43cdca97896611b3fd5c7498db1cecbfef658c04
santiagoropero <santiagoropero2001@gmail.com>
2022-05-22 20:26:35 -0500

    Inicio desarrollo de component list-students

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ListStudentComponent} from './list-student/list-student.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/students', pathMatch: 'full'},
+  {path: 'students', component: ListStudentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
